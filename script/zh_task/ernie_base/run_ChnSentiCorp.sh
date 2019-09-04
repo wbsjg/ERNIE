d3ac95ded735ddd4ef742766f51d6ea3c57b7cf1
wbsjg <38601392+wbsjg@users.noreply.github.com>
2019-09-04 17:20:48 +0800

    Update run_ChnSentiCorp.sh

--- script/zh_task/ernie_base/run_ChnSentiCorp.sh
+++ script/zh_task/ernie_base/run_ChnSentiCorp.sh
@@ -17,6 +17,7 @@ python -u run_classifier.py \
                    --train_set ${TASK_DATA_PATH}/chnsenticorp/train.tsv \
                    --dev_set ${TASK_DATA_PATH}/chnsenticorp/dev.tsv,${TASK_DATA_PATH}/chnsenticorp/test.tsv \
                    --test_set ${TASK_DATA_PATH}/chnsenticorp/test_1.tsv \
+                   --test_save ${TASK_DATA_PATH}/chnsenticorp/test_out.tsv \
                    --vocab_path ${MODEL_PATH}/vocab.txt \
                    --checkpoints ./checkpoints \
                    --save_steps 1000 \
